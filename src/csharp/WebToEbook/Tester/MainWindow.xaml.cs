d4ab5e50fd093f5db69479fb84911d21f6209b53
Bijeshnath <bijeshnath@gmail.com>
2013-04-28 21:05:15 +0100

    Updated test app

--- src/csharp/WebToEbook/Tester/MainWindow.xaml.cs
+++ src/csharp/WebToEbook/Tester/MainWindow.xaml.cs
@@ -31,25 +31,23 @@ namespace Tester
         private const string kindleGenExe = @"C:\Users\Home\Documents\Reader\kindlegen\kindlegen.exe";
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            string path = @"C:\Users\Home\Documents\Reader\";
+            string path = txtOutputDir.Text;
             List<RemoveNode> removeNodes = new List<RemoveNode>();
             RemoveNode rn = new RemoveNode("script", null, null);
             removeNodes.Add(rn);
             rn = new RemoveNode("div", "class", "portal");
             removeNodes.Add(rn);
-            Downloader dn = new Downloader(path, @"http://en.wikipedia.org/", removeNodes, "1.html");
+            Downloader dn = new Downloader(path,txtUrl.Text, removeNodes, txtOutputFile.Text + ".html");
             dn.Download();
             string str = "\"" + path + dn.FileName + "\"";
             ProcessStartInfo psi = new ProcessStartInfo(kindleGenExe, str);
-           // psi.RedirectStandardOutput = true;
-           // psi.RedirectStandardError = true;
+
             psi.UseShellExecute = false;
             Process p = new Process();
             p.StartInfo = psi;
             p.Start();
             p.WaitForExit();
-         //   string strOut = p.StandardOutput.ReadToEnd();
-          //  string strErr = p.StandardError.ReadToEnd();
+
 
 
         }
